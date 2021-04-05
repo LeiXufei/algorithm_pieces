aaa7f8262fe6bae586a71539f5bbe0ba4a14e8a3
leixufei <leixufei@gmail.com>
2021-04-05 11:08:15 +0800

    add multiplicative_inverse code

---
+++ multiplicative_inverse_by_MOD/multiplicative_inverse_by_MOD.cpp
@@ -0,0 +1,34 @@
+// y^-1 = y^(MOD-2) % MOD when MOD is prime
+
+const int MOD = 998244353;// example prime MOD
+
+int add(int x, int y)
+{
+    x += y;
+    while(x >= MOD) x -= MOD;
+    while(x < 0) x += MOD;
+    return x;
+}
+
+int mul(int x, int y)
+{
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y)
+{
+    int ans = 1;
+    while(y > 0)
+    {
+        if(y % 2 == 1)
+            ans = mul(ans, x);
+        x = mul(x, x);
+        y /= 2;
+    }
+    return ans;
+}
+
+int divide(int x, int y)
+{
+    return mul(x, binpow(y, MOD ¡ª 2));
+}
